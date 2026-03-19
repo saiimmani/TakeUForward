8facb0e2a471b722d006f8f5e91722c88dc1925e
Immani Rama Venkata Sri Sai <saichowdaryimmani@gmail.com>
2026-03-19 12:13:52 +0530

    Add pattern printing solution in P2.java

---
+++ Pattern Problems/P2.java
@@ -0,0 +1,30 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+class Solution {
+    public void pattern1(int n) {
+        for (int i = 1; i<=n; i++){ 
+            for (int j = 1; j<=i; j++){ // in inner loop we just modified 'j<=i' just to print * according to rows number 👇 refer ouput
+                System.out.print("*"); 
+            }
+            System.out.println();
+        }
+
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        sol.pattern1(5);
+    }
+}
+
+
+/* 
+  Output 👇
+*
+**
+***
+****
+*****
+*/
