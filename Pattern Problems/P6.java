7ec62e44d8b70342851fcb0d4c3f70c805f4d369
Immani Rama Venkata Sri Sai <saichowdaryimmani@gmail.com>
2026-03-19 22:50:43 +0530

    Update pattern1 method to print numbers twice
    Modified the pattern1 method to print the same number twice in each row.

---
+++ Pattern Problems/P6.java
@@ -0,0 +1,28 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+class Solution {
+    public void pattern1(int n) {
+        for (int i = n; i>=1; i--){
+            for (int j = 1; j <= i; j++){
+                System.out.print(j);          // same patter just printing j in this case
+            }
+            System.out.println();
+        }
+
+    }
+}
+
+class P6 {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        sol.pattern1(5);
+    }
+}
+
+/* 
+12345
+1234
+123
+12
+1
+*/ 
