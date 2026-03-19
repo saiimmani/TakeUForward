1d19e392d6e80a2298a71d001c96365cb8e63d46
Immani Rama Venkata Sri Sai <saichowdaryimmani@gmail.com>
2026-03-19 22:51:45 +0530

    Similar to P5 same logic

--- Pattern Problems/P6.java
+++ Pattern Problems/P6.java
@@ -4,7 +4,7 @@ class Solution {
     public void pattern1(int n) {
         for (int i = n; i>=1; i--){
             for (int j = 1; j <= i; j++){
-                System.out.print(j);          // same patter just printing j in this case
+                System.out.print(j);          // same patter just printing j in this case as 👇
             }
             System.out.println();
         }
