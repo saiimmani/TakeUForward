b656b0c28bc395644e3f0bb119cd4bb4433fecd1
Immani Rama Venkata Sri Sai <saichowdaryimmani@gmail.com>
2026-03-19 12:07:22 +0530

    Enhance P1.java with explanatory comments
    Added comments to explain the pattern generation logic.

--- Pattern Problems/P1.java
+++ Pattern Problems/P1.java
@@ -3,20 +3,20 @@
 
 class Solution {
     public void pattern1(int n) {
-        for (int i = 1; i<=n; i++){
-            for (int j = 1; j<=n; j++){
+        for (int i = 1; i<=n; i++){ // Here "i" represents rows , outer loop waits for inner loop to run and increments its value and move to i+1 i.e 2 row 
+            for (int j = 1; j<=n; j++){ // Here 'j' represents cols, it complete all five iterations and print '*' in five cols -> *****.
                 System.out.print("*");
-            }
+            }// complete the inner loop
             System.out.println();
-        }
+        } // comple=te outer loop
 
-    }
-}
+    } // end of method 
+} // end of class
 
 class P1 {
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-        sol.pattern1(5);
+    public static void main(String[] args) { // Main Function
+        Solution sol = new Solution(); // Create obj to class the method 
+        sol.pattern1(5); // assign 5 as input to method
     }
 }
 
