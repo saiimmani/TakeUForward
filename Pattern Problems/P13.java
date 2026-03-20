f9bea5d4668412b79bcd902a8ab8f488109061cd
Immani Rama Venkata Sri Sai <saichowdaryimmani@gmail.com>
2026-03-20 20:34:19 +0530

    Add pattern printing solution in P13.java
    Implement pattern printing in P13.java to display a number pyramid.

---
+++ Pattern Problems/P13.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void pattern(int n) {
+       int num = 1;
+    for (int i=1 ; i<=n; i++){
+        for (int j=1;j<=i;j++){
+            System.out.print(num+" ");
+            num+=1;
+        }
+         System.out.println();
+    }
+}
+  
+}
+class P13 {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        sol.pattern(5);
+    }
+}
+
+/*
+
+1 
+2 3 
+4 5 6 
+7 8 9 10 
+11 12 13 14 15 
+
+*/
