8b2386749ec515fe737e70f7f6c833ebbffba14e
Immani Rama Venkata Sri Sai <saichowdaryimmani@gmail.com>
2026-03-20 20:50:31 +0530

    Add pattern printing solution in P14.java
    Implement a pattern printing solution that outputs letters in a right-angled triangle format.

---
+++ Pattern Problems/P14.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void pattern(int n) {
+    for (int i=0 ; i<n; i++){
+        for (char ch = 'A' ; ch <= 'A'+ i; ch++){  // ASCI , we are incrementing the Character Same as Right Angled Triangle
+            System.out.print(ch+" ");
+        }
+         System.out.println();
+    }
+
+ }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        sol.pattern(5);
+    }
+}
+
+/*
+A 
+A B 
+A B C 
+A B C D 
+A B C D E 
+
+*/
