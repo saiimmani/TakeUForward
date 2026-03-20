2dc71f4fdb24f380bc5503b257a5f7f96570ec4a
Immani Rama Venkata Sri Sai <saichowdaryimmani@gmail.com>
2026-03-21 00:42:16 +0530

    Add pattern printing method in Solution class
    Implement a method to print a specific character pattern based on input size.

---
+++ Pattern Problems/P17.java
@@ -0,0 +1,47 @@
+class Solution {
+    public void pattern(int n) {
+    for (int i=0 ; i<n; i++){
+    
+        for (int j = 0; j<n-i-1; j++){
+            System.out.print(" ");
+        }
+        char ch = 'A';
+        int breakpoint = (2*i+1)/2;
+        for (int k = 1; k<=(2*i)+1; k++){
+            System.out.print(ch);
+            if(k <= breakpoint){
+                
+                ch++;
+                
+            }
+            else{
+                ch--;
+                
+            }
+        }
+        
+        for (int m = 0; m<n-i-1; m++){
+            System.out.print(" ");
+        }
+         System.out.println();
+    }
+
+ }
+}
+
+class P17 {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        sol.pattern(5);
+    }
+}
+
+/*
+
+    A    
+   ABA   
+  ABCBA  
+ ABCDCBA 
+ABCDEDCBA
+
+*/
