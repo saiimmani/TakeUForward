017fc53b700c0ff5ba70d1c589452459172dc6b7
Immani Rama Venkata Sri Sai <saichowdaryimmani@gmail.com>
2026-03-21 14:07:24 +0530

    Add pattern printing functionality in P20.java
    Implement a pattern printing class with upper and lower halves.

---
+++ Pattern Problems/P20.java
@@ -0,0 +1,63 @@
+class Solution {
+    public void pattern(int n) {
+
+        // Upper half
+        for (int i = 1; i <= n; i++) {
+
+            // Left stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            // Spaces
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+
+            // Right stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        // Lower half
+        for (int i = n - 1; i >= 1; i--) {
+
+            // Left stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            // Spaces
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+
+            // Right stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
+class Main {
+    public static void main(String[] args) {
+        new Solution().pattern(4);
+    }
+}
+
+/*
+
+*      *
+**    **
+***  ***
+********
+***  ***
+**    **
+*      *
+
+*/
