4430e8fe4b37c7cf24c4347846f18fe152284a56
Immani Rama Venkata Sri Sai <saichowdaryimmani@gmail.com>
2026-03-21 14:46:45 +0530

    Refactor P21.java to remove spaces in pattern

--- Pattern Problems/P21.java
+++ Pattern Problems/P21.java
@@ -5,9 +5,9 @@ class Solution {
             for (int j = 1; j <= n; j++) {
 
                 if (i == 1 || i == n || j == 1 || j == n) {
-                    System.out.print("* ");
+                    System.out.print("*");
                 } else {
-                    System.out.print("  ");
+                    System.out.print(" ");
                 }
 
             }
@@ -26,9 +26,8 @@ class Main {
 
 /*
 
-* * * * 
-*     * 
-*     * 
-* * * * 
-
+****
+*  *
+*  *
+****
 */
