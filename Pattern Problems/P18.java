9e27213ebc5270d4b870a00cb9f9b8b4de4f84f7
Immani Rama Venkata Sri Sai <saichowdaryimmani@gmail.com>
2026-03-21 10:48:27 +0530

    Add pattern printing solution in P18.java
    Implement a pattern printing solution that displays letters in descending order from 'E' to 'A'. The pattern is generated based on the input number of rows.

--- Pattern Problems/P18.java
+++ Pattern Problems/P18.java
@@ -1,1 +1,26 @@
+class Solution {
+    public void pattern(int n) {
+    for (int i=0 ; i<n; i++){
+    for (char ch = (char)('E'- i); ch <= 'E'; ch++){
+         System.out.print(ch+" ");
+    }
+           System.out.println(); 
+ }
+}
+}
 
+class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        sol.pattern(5);
+    }
+}
+
+/*
+E 
+D E 
+C D E 
+B C D E 
+A B C D E 
+
+*/
