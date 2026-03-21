55ec9757abc65551de446c7666656590cc483140
Immani Rama Venkata Sri Sai <saichowdaryimmani@gmail.com>
2026-03-21 14:45:50 +0530

    Add pattern printing solution in P21.java
    Implement a pattern printing solution that prints a square border of stars for a given size.

---
+++ Pattern Problems/P21.java
@@ -0,0 +1,34 @@
+class Solution {
+    public void pattern(int n) {
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= n; j++) {
+
+                if (i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+
+            }
+
+            System.out.println();
+        }
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Solution obj = new Solution();
+        obj.pattern(4);
+    }
+}
+
+/*
+
+* * * * 
+*     * 
+*     * 
+* * * * 
+
+*/
