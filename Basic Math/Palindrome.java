02a3bdf8ddaea0bdcfe7e5bc270123a7a75b3c33
Immani Rama Venkata Sri Sai <saichowdaryimmani@gmail.com>
2026-03-22 17:25:39 +0530

    Implement palindrome check in Palindrome.java

--- Basic Math/Palindrome.java
+++ Basic Math/Palindrome.java
@@ -1,1 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+       int rev = 0;
+       int copy = x; // saving copy of the given integer
+       while( x != 0){
+        int digit = 0;
+        if (x < 0) return false;
+        digit = x%10;   // saves last digit 
+        rev = rev*10 + digit;      // save reverse 
+        x = x/10;   
 
+       } 
+        return rev == copy;
+    }
+}
