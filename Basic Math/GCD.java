8cb697f8b46653a754b5801191d0f9551808bdaa
Immani Rama Venkata Sri Sai <saichowdaryimmani@gmail.com>
2026-03-22 17:45:45 +0530

    Add GCD calculation functionality in GCD.java

---
+++ Basic Math/GCD.java
@@ -0,0 +1,37 @@
+
+import java.util.*;
+
+public class Main {
+    public static int findGcd(int n1, int n2) {
+        // Iterate from the minimum of
+        // n1 and n2 down to 1
+        // Start from the minimum of n1 and n2
+        // because the GCD cannot
+        // exceed the smaller number
+        
+        for (int i = Math.min(n1, n2); i > 0; i--) {
+            // Check if i is a common
+            // factor of both n1 and n2
+            if (n1 % i == 0 && n2 % i == 0) {
+                // If i is a common factor,
+                // return it as the GCD
+                return i;
+            }
+        }
+        // If no common factors are found,
+        // return 1 (as 1 is always a
+        // divisor of any number)
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        int n1 = 20, n2 = 15;
+        
+        // Find the GCD of n1 and n2
+        int gcd = findGcd(n1, n2);
+
+        System.out.println("GCD of " + n1 + " and " + n2 + " is: " + gcd);
+    }
+}
+    
+                                
