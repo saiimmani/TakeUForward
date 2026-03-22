a031e6da6f20866ac1a1f53cddbc57763537c7f8
Immani Rama Venkata Sri Sai <saichowdaryimmani@gmail.com>
2026-03-22 19:24:10 +0530

    Implement Armstrong number checker in Armstrong.java
    Added a class to check for Armstrong numbers with a static method.

--- Basic Math/Armstrong.java
+++ Basic Math/Armstrong.java
@@ -1,1 +1,28 @@
+public class ArmstrongChecker {
 
+    // Static method to check if a number is an Armstrong number
+    public static boolean isArmstrong(int num) {
+        int k = String.valueOf(num).length(); // Get number of digits
+        int sum = 0;
+        int n = num;
+
+        while (n > 0) {
+            int ld = n % 10;             // Last digit
+            sum += Math.pow(ld, k);      // Add ld^k
+            n /= 10;                     // Remove digit
+        }
+
+        return sum == num;
+    }
+
+    public static void main(String[] args) {
+        int number = 153;
+
+        // Use class method to check
+        if (isArmstrong(number)) {
+            System.out.println(number + " is an Armstrong number.");
+        } else {
+            System.out.println(number + " is not an Armstrong number.");
+        }
+    }
+}
