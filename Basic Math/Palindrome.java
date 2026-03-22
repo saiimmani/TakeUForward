9f486c7cbc7783fdc1ab0f104af9c1b0830cff0f
Immani Rama Venkata Sri Sai <saichowdaryimmani@gmail.com>
2026-03-22 17:33:35 +0530

    Refactor isPalindrome method for negative check

--- Basic Math/Palindrome.java
+++ Basic Math/Palindrome.java
@@ -2,9 +2,9 @@ class Solution {
     public boolean isPalindrome(int x) {
        int rev = 0;
        int copy = x; // saving copy of the given integer
-       while( x != 0){
-        int digit = 0;
         if (x < 0) return false;
+        while( x > 0){
+        int digit = 0;
         digit = x%10;   // saves last digit 
         rev = rev*10 + digit;      // save reverse 
         x = x/10;   
