d5e62cf5d55169294a988490bf9fc3f60cb3db04
Immani Rama Venkata Sri Sai <saichowdaryimmani@gmail.com>
2026-06-03 12:52:58 +0530

    Refactor selection sort variable names
    Refactor variable names for clarity and fix typos.

--- Sorting/selection.java
+++ Sorting/selection.java
@@ -3,15 +3,18 @@ class selection {
     static void sort(int[] arr) {
     int n = arr.length;
     for ( int i = 0; i < n-1; i++){
-       int MinIndex = i;
+       int minIndex = i;
     for ( int j = i+1 ; j < n ; j++){
-            if(arr[j] < arr[MinIndex]){
-                MinIndex = j;
+            if(arr[j] < arr[minIndex]){
+                minIndex = j;
             
         }
     }
-        int temp = arr[MinTndex];
-        arr[MinTndex] = arr[i];
+        int temp = arr[minTndex];
+        arr[minTndex] = arr[i];
         arr[i] = temp;
         
+}
+    }
+
 }
