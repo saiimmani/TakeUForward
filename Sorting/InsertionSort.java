3b23e57e1da40f1c20be710919598759beefed1f
Immani Rama Venkata Sri Sai <saichowdaryimmani@gmail.com>
2026-06-04 09:16:29 +0530

    Implement InsertionSort class and update main method

---
+++ Sorting/InsertionSort.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class InsertionSort {
+    public int[] insertionSort(int[] nums) {
+        int n = nums.length; // Size of the array 
+        
+        // For every element in the array 
+        for (int i = 1; i < n; i++) {
+            int key = nums[i]; // Current element as key 
+            int j = i - 1;
+            
+            // Shift elements that are greater than key by one position
+            while (j >= 0 && nums[j] > key) {
+                nums[j + 1] = nums[j];
+                j--;
+            }
+            
+            nums[j + 1] = key; // Insert key at correct position
+        }
+        
+        return nums;
+    }
+}
+
+
+class Main {
+    public static void main(String[] args) {
+        // Create an instance of solution class
+        InsertionSort InsertionSort = new InsertionSort();
+        
+        int[] nums = {13, 46, 24, 52, 20, 9};
+        
+        System.out.println("Before Using Insertion Sort: ");
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+        
+        // Function call for insertion sort
+        nums = InsertionSort.insertionSort(nums);
+        
+        System.out.println("After Using Insertion Sort: ");
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
